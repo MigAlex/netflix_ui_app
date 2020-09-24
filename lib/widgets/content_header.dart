69267ffa38ddc4c5cbe96ace34c39f2bfb354829
Alex <awangardowie@gmail.com>
2020-09-24 19:19:15 +0200

    playing video in content header

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_responsive_ui/widgets/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 import '../models/models.dart';
 
@@ -7,6 +8,19 @@ class ContentHeader extends StatelessWidget {
   final Content featureContent;
 
   const ContentHeader({Key key, this.featureContent}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _ContentHeaderMobile(featureContent: featureContent),
+      desktop: _ContentHeaderDesktop(featureContent: featureContent),
+    );
+  }
+}
+
+class _ContentHeaderMobile extends StatelessWidget {
+  final Content featureContent;
+
+  const _ContentHeaderMobile({Key key, this.featureContent}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -57,11 +71,153 @@ class ContentHeader extends StatelessWidget {
   }
 }
 
+class _ContentHeaderDesktop extends StatefulWidget {
+  final Content featureContent;
+
+  const _ContentHeaderDesktop({Key key, this.featureContent}) : super(key: key);
+
+  @override
+  __ContentHeaderDesktopState createState() => __ContentHeaderDesktopState();
+}
+
+class __ContentHeaderDesktopState extends State<_ContentHeaderDesktop> {
+  VideoPlayerController _videoPlayerController;
+  bool _isMuted = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoPlayerController =
+        VideoPlayerController.network(widget.featureContent.videoUrl)
+          ..initialize().then((_) => setState(() {}))
+          ..setVolume(0)
+          ..play();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _videoPlayerController.value.isPlaying
+          ? _videoPlayerController.pause()
+          : _videoPlayerController.play(),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          AspectRatio(
+            aspectRatio: _videoPlayerController.value.initialized
+                ? _videoPlayerController.value.aspectRatio
+                : 2.344,
+            child: _videoPlayerController.value.initialized
+                ? VideoPlayer(_videoPlayerController)
+                : Image.asset(
+                    widget.featureContent.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: -1.0,
+            child: AspectRatio(
+              aspectRatio: _videoPlayerController.value.initialized
+                  ? _videoPlayerController.value.aspectRatio
+                  : 2.344,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black, Colors.transparent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 60.0,
+            right: 60.0,
+            bottom: 150.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250.0,
+                  child: Image.asset(widget.featureContent.titleImageUrl),
+                ),
+                const SizedBox(height: 15.0),
+                Text(
+                  widget.featureContent.description,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w500,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(2.0, 4.0),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  children: [
+                    _PlayButton(),
+                    const SizedBox(width: 16.0),
+                    FlatButton.icon(
+                      padding:
+                          const EdgeInsets.fromLTRB(26, 11, 32, 8),
+                      onPressed: () => print('More Info'),
+                      color: Colors.white,
+                      icon: const Icon(Icons.info_outline, size: 30.0),
+                      label: const Text(
+                        'More Info',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20.0),
+                    if (_videoPlayerController.value.initialized)
+                      IconButton(
+                        icon: Icon(
+                          _isMuted ? Icons.volume_off : Icons.volume_up,
+                        ),
+                        color: Colors.white,
+                        iconSize: 30.0,
+                        onPressed: () => setState(() {
+                          _isMuted
+                              ? _videoPlayerController.setVolume(100)
+                              : _videoPlayerController.setVolume(0);
+                          _isMuted = _videoPlayerController.value.volume == 0;
+                        }),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class _PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
-        padding: const EdgeInsets.fromLTRB(16, 5, 21, 5),
+        padding: !Responsive.isDesktop(context)
+            ? const EdgeInsets.fromLTRB(16, 5, 21, 5)
+            : const EdgeInsets.fromLTRB(26, 11, 32, 8),
         onPressed: () => print('Play'),
         color: Colors.white,
         label: const Text(
