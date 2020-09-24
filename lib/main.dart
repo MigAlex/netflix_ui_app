ca1cfa72bd2c646def592c04c7a5549499e6d253
Alex <awangardowie@gmail.com>
2020-09-24 17:45:30 +0200

    bottom navibar added

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         scaffoldBackgroundColor: Colors.black
       ),
-      home: Home(),
+      home: NavScreen()
     );
   }
 }
